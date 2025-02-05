01daf6b5a58a1232cb9f4094bf653a8e8f030fb7
monjane10 <louramonja52.n@gmail.com>
2025-02-05 16:53:11 +0200

    conectando com a base de dados

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import app from './src/app.js'
+import conexao from './infra/conexao.js';
 
 const PORTA = 3000;
 
-
-app.listen(PORTA, () =>{
-    console.log(`Servidor rodando na porta: https://localhost:${PORTA}`);
- });
+// conectando com o banco de dados
+conexao.connect((erro) => {
+   if (erro) {
+       console.log('Erro ao conectar com a Base de dados' + erro);
+   } else {
+       console.log('Conexão com a Base de dados realizada com sucesso');
+       //Escutar a porta 3000
+       app.listen(PORTA, () =>{
+        console.log(`Servidor rodando na porta: https://localhost:${PORTA}`);
+     });
+   }
+});
