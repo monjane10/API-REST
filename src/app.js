092994aed0e1fa1b0d1e507fc67c44727dabb66f
monjane10 <louramonja52.n@gmail.com>
2025-02-11 14:43:00 +0200

    finalizando

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import selecaoController from './app/controllers/selecaoController.js';
+import routes from './routes.js'
+
 
 const app = express();
+
+//usar o express
 app.use(express.json()); 
 
-//ROTAS
-app.get('/selecoes', selecaoController.index);
-app.get('/selecoes/:id', selecaoController.show);
-app.post('/selecoes', selecaoController.store );
-app.put('/selecoes/:id', selecaoController.update );
-app.delete('/selecoes/:id', selecaoController.delete );
+//Usar o router
+app.use(routes);
+
+
   
 export default app;
