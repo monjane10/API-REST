66b305b8388740405e64bf896ab5b9a18e1b77b9
monjane10 <louramonja52.n@gmail.com>
2025-02-11 11:43:38 +0200

    refactorando o codigo para o padrão MVC

--- src/app.js
+++ src/app.js
@@ -1,72 +1,14 @@
 import express from 'express';
-import conexao from '../infra/conexao.js';
+import selecaoController from './app/controllers/selecaoController.js';
 
 const app = express();
 app.use(express.json()); 
 
-
-app.post('/selecoes', (req, res) => {
-    const selecao = req.body; 
-    const sql = "INSERT INTO selecoes SET?;"
-    conexao.query(sql, selecao, (erro, resultado) => {
-        if (erro){
-            res.status(400).json({'Erro': 'Erro ao adicionar seleção!', erro});
-        } else {
-            res.status(201).json(resultado);
-        }
-    });
- });
-
-app.get('/selecoes', (req, res) =>{
-    const sql = "SELECT * FROM selecoes;"
-    conexao.query(sql, (erro, resultado) => {
-        if (erro){
-            res.status(404).json({'Erro': 'Dados não encontrados!', erro});
-        } else {
-            res.status(200).json(resultado);
-        }
-    });
-   
-});
-
-app.get('/selecoes/:id', (req, res) => {
-    const id = parseInt(req.params.id); 
-    const sql = "SELECT * FROM selecoes WHERE id=?;"
-    conexao.query(sql, id, (erro, resultado) => {
-        const linha = resultado[0]
-        if (erro){
-            res.status(404).json({'Erro': 'Dados não encontrados!', erro});
-        } else {
-            res.status(200).json(linha);
-        }
-    });
-  });
-
-
-  app.put('/selecoes/:id', (req, res) => { 
-    const id = parseInt(req.params.id); 
-    const selecao = req.body; 
-    const sql = "UPDATE selecoes SET? WHERE id=?;"
-    conexao.query(sql, [selecao, id], (erro, resultado) => {
-        if (erro){
-            res.status(400).json({'Erro': 'Erro ao actualizar seleção!', erro});
-        } else {
-            res.status(200).json(resultado);
-        }
-    });
-  });
-
-
-  app.delete('/selecoes/:id', (req, res) => {
-    const id = parseInt(req.params.id); 
-    const sql = "DELETE FROM selecoes WHERE id=?;"
-    conexao.query(sql, id, (erro, resultado) => {
-        if (erro){
-            res.status(404).json({'Erro': 'Dados não encontrados!', erro});
-        } else {
-            res.status(200).json(resultado);
-        }
-    });
-  });
+//ROTAS
+app.get('/selecoes', selecaoController.index);
+app.get('/selecoes/:id', selecaoController.show);
+app.post('/selecoes', selecaoController.store );
+app.put('/selecoes/:id', selecaoController.update );
+app.delete('/selecoes/:id', selecaoController.delete );
   
 export default app;
